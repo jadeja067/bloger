847d8a1902f1818d00129cb2ca16e3ab1ce2b9a6
Kuldipsinh Jadeja <69837239+jadeja067@users.noreply.github.com>
2023-12-14 13:21:30 +0530

    Update index.js

--- schema/blog/index.js
+++ schema/blog/index.js
@@ -1,24 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-// const BlogSchema = new Schema({
-//   title: {
-//     type: String,
-//     require: true
-//   },
-//   description: {
-//     type: String,
-//     require: true
-//   },
-//   uid: {
-//     type: String,
-//     require: true
-//   }
-// });
 const BlogSchema = new Schema({
-  title: String,
-  description: String,
-  uid: String
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  uid: {
+    type: String,
+    required: true
+  }
 });
 
 exports.blogschema = mongoose.model("Blogs", BlogSchema);
