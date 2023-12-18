2fffea93a722a37dce96fd75df5b916f9f4d9af8
Kuldipsinh Jadeja <69837239+jadeja067@users.noreply.github.com>
2023-12-18 10:54:33 +0530

    Update index.js

--- controller/user/index.js
+++ controller/user/index.js
@@ -21,9 +21,9 @@ exports.getAllItems = async (req, res) => {
 };
 
 exports.getOneItem = async (req, res) => {
-  const id = req.params.id;
+  const id = req.params.username;
   try {
-    const user = await userschema.findById(id);
+    const user = await userschema.findOne({username: id});
     res.json(user).status(200);
   } catch (e) {
     res.status(e);
@@ -44,9 +44,9 @@ exports.createItem = async (req, res) => {
 };
 
 exports.updateItem = async (req, res) => {
-  const id = req.params.id;
+  const id = req.params.username;
   try {
-    let user = await userschema.findById(id);
+    let user = await userschema.findOne({username: id});
     await user.updateOne(req.body);
     res.json({
       status: "Success",
@@ -58,9 +58,9 @@ exports.updateItem = async (req, res) => {
 };
 
 exports.DeleteItem = async (req, res) => {
-  const id = req.params.id;
+  const id = req.params.username;
   try {
-    const user = await userschema.findById(id);
+    const user = await userschema.findOne({username: id});
     await user.deleteOne();
     res.status(200).json({
       status: "Success",
