d37bc076513608ab9df022da8ec31a41821421df
Purvi kargathara <purvi.devmonk@gmail.com>
2023-12-18 10:38:33 +0530

    cc

--- schema/user/index.js
+++ schema/user/index.js
@@ -5,18 +5,12 @@ const UserSchema = new Schema({
   username: {
     type: String,
     required: true,
-<<<<<<< HEAD
     // unique: true,
-=======
->>>>>>> 599cbd910d58c574ab223ff33a2ff9c90e636a7b
   },
   email: {
     type: String,
     required: true,
-<<<<<<< HEAD
     
-=======
->>>>>>> 599cbd910d58c574ab223ff33a2ff9c90e636a7b
   },
   password: {
     type: String,
